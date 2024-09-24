cb9e507542b4ed01b7b960436181f8e41721528a
Gray <cheloghm@gmail.com>
2024-09-25 00:44:22 +0100

    Refactored code base, and moved code files to appropriate directories.

--- Mongo-seed/init-mongo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Seeding MongoDB with Titanic data..."
-mongoimport --host localhost --db PoseidonDB --collection Passengers --type csv --headerline --file /docker-entrypoint-initdb.d/titanic.csv
